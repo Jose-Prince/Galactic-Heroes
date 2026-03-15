dbfaeaf640450b37dfa48bbe3e07cde4720c65ad
Jose-Prince <joseprince148@gmail.com>
2026-03-15 17:07:18 -0600

    Feat: Ship stats

---
+++ Assets/Scripts/ShipSettings.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipSettings : MonoBehaviour
+{
+    [Header("Values")]
+    [SerializeField] float totalHealth = 100f;
+    [SerializeField] float totalShield = 20f;
+    [SerializeField] float totalBoost = 20f;
+
+    [Header("Bars")]
+    [SerializeField] Image healthBar;
+    [SerializeField] Image shieldBar;
+    [SerializeField] Image boostBar;
+
+    private float currentHealth;
+    private float currentShield;
+    private float currentBoost;
+
+    void Start()
+    {
+        currentHealth = totalHealth;
+        currentBoost = totalBoost;
+        currentShield = totalShield;    
+    }
+
+    public void Heal(float amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, totalHealth);
+
+        healthBar.fillAmount = currentHealth / totalHealth;
+    }
+
+    public float GetHealth()
+    {
+        return currentHealth;
+    }
+
+    public float GetMaxHealth()
+    {
+        return totalHealth;
+    }
+
+    public void HealShield(float amount)
+    {
+        currentShield += amount;
+        currentShield = Mathf.Clamp(currentShield, 0, totalShield);
+
+        shieldBar.fillAmount = currentShield / totalShield;
+    }
+
+    public float GetShield()
+    {
+        return currentShield;
+    }
+
+    public float GetMaxShield()
+    {
+        return totalShield;
+    }
+
+    public void HealBoost(float amount)
+    {
+        currentBoost += amount;
+        currentBoost = Mathf.Clamp(currentBoost, 0, totalBoost);
+
+        boostBar.fillAmount = currentBoost / totalBoost;
+    }
+
+    public float GetBoost()
+    {
+        return currentBoost;
+    }
+
+    public float GetMaBoost()
+    {
+        return totalBoost;
+    }
+}
