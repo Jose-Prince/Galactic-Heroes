37c6adc8cd1bb850ce854d4ab64f9963fa320955
Jose-Prince <joseprince148@gmail.com>
2026-04-24 00:27:26 -0600

    Feat: initial horizontal movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speedMultAngle = 0.5f;
     [SerializeField] float speedRollMultAngle = 0.05f;
     [SerializeField] float maxRollAngle = 45f;
+    [SerializeField] GameObject horizontalPivot;
 
     [SerializeField] StatsData stats;
 
@@ -42,6 +44,7 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         HandleMovement();
+        HandleTurning();
         HandleRotation();
         HandleRoll();
     }
@@ -60,19 +63,55 @@ public class PlayerController : MonoBehaviour
         }
 
         Vector3 forwardVelocity = -transform.right * currentSpeed;
-
-        Vector3 sideVelocity = transform.forward * horizontalMove * stats.handling;
-
-        rb.linearVelocity = forwardVelocity + sideVelocity;
+        rb.linearVelocity = forwardVelocity;
     }
 
     void HandleRotation()
     {
-        
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            rb.AddTorque(-transform.right * speedMultAngle * mouseInputY * -1, ForceMode.VelocityChange);
+            rb.AddTorque(transform.up * speedMultAngle * mouseInputX * stats.handling, ForceMode.VelocityChange);
+        }
     }
 
     void HandleRoll()
     {
-        
+        float currenRoll = Vector3.SignedAngle(Vector3.up, transform.up, transform.forward);
+
+        if ((rollInput > 0 && currenRoll < maxRollAngle) || 
+            (rollInput < 0 && currenRoll > -maxRollAngle))
+        {
+            rb.AddTorque(-transform.right * speedMultAngle * rollInput, ForceMode.VelocityChange);
+        }
+
+        if (Mathf.Abs(currenRoll) >= maxRollAngle)
+        {
+            Vector3 angVel = rb.angularVelocity;
+            angVel -= Vector3.Project(angVel, transform.right);
+            rb.angularVelocity = angVel;
+        }
+    }
+
+    void HandleTurning()
+    {
+        if (horizontalPivot == null) return;
+
+        float turnInput = horizontalMove;
+        float speedFactor = currentSpeed / stats.speed;
+
+        float turnSpeed = turnInput * stats.handling * speedFactor;
+
+        Vector3 offset = transform.position - horizontalPivot.transform.position;
+
+        Quaternion rotation = quaternion.Euler(0, turnSpeed, 0);
+
+        offset = rotation * offset;
+
+        Vector3 newPosition = horizontalPivot.transform.position + offset;
+
+        rb.MovePosition(newPosition);
+
+        rb.MoveRotation(rotation * rb.rotation);
     }
 }
