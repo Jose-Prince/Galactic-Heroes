ed5bfbc33c3a7f545d0d17ee073d6994ec03bfef
Jose-Prince <joseprince148@gmail.com>
2026-05-04 05:09:55 -0600

    Fix: get current scene when starting game

--- Assets/Scripts/SceneFlowManager.cs
+++ Assets/Scripts/SceneFlowManager.cs
@@ -6,6 +6,27 @@ public class SceneFlowManager : MonoBehaviour
 {
     string currentScene;
 
+    void Awake()
+    {
+        DetectCurrentScene();
+    }
+
+    void DetectCurrentScene()
+    {
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene scene = SceneManager.GetSceneAt(i);
+
+            if (scene.name != "Bootstrap")
+            {
+                currentScene = scene.name;
+                SceneManager.SetActiveScene(scene);
+                break;
+            }
+        }
+        Debug.Log("Scene: " + currentScene);
+    }
+
     public void LoadScene(string sceneName)
     {
         StartCoroutine(LoadRoutine(sceneName));
